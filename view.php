0eb06067a4a3c39dbf3e7e40c7058d212d532121
Athar Mohammad <82324903+Athar996@users.noreply.github.com>
2023-04-04 23:53:33 +0530

    Update view.php

--- view.php
+++ view.php
@@ -1,3 +1,24 @@
+<?php
+    require 'asqlphpconn.php';
+    if($_SERVER['REQUEST_METHOD']=="POST")
+    {
+        $firstname = $_POST['delname'];
+        $delqry = "Delete from details where firstname='$firstname'";
+        $delrun = mysqli_query($conn, $delqry);
+        if(mysqli_affected_rows($conn)>0)
+        {
+            echo  '<script> 
+                                alert("Contact Deleted")
+                    </script>';
+        }
+        else
+        {
+            echo  '<script> 
+                        alert("Mis-match firstname PLease try again")
+                    </script>';
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,24 +27,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css">
-    <title>display contact</title>
+    <title>Display contact</title>
 </head>
 
 <body>
     <div class="viewcontparent">
-        <h2>Displaying contact list</h2>
+        <h1>Contact list</h1>
         <div class="cont">
             <?php
-            $database = "contactbook";
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-
-            $conn = mysqli_connect($servername, $username, $password, $database);
             $qry = "SELECT * FROM contactbook.details;";
             $run = mysqli_query($conn, $qry);
-
-            while ($array = mysqli_fetch_array($run)) {
+            
+            while ($array = mysqli_fetch_array($run)) 
+            {
                 echo "Firstname:- ", $array[0], "<br>";
                 echo "Lastname:- ", $array[1], "<br>";
                 echo "Number:- ",  $array[2], "<br>";
@@ -31,48 +47,26 @@
             }
             ?>
         </div>
-       <h2> <a href="add.html">Want to add some more contacts?</a></h2>
+        <div class="options">
+            <h2> <a href="add.html">Add some more contacts?</a></h2>
+            <div class="delete" onclick="del()">
+                <h2>Delete contact?</h2>
+                <form action="" method="POST">
+                    <input type="text" placeholder="Firstname" name="delname" id="delete" hidden>
+                    <input type="submit" id="delbtn" value="Delete" hidden>
+                </form>
+            </div>
+        </div>
     </div>
 
 
-
-
-    <!-- 
- <table>
-        <tr>
-            <th>FirstName</th>
-            <th>LastName</th>
-            <th>Number</th>
-            <th>E-mail Id </th>
-        </tr> -->
-
-
-        <!-- ?> -->
-        <!-- <table>
-            <tr>
-                <td>
-                <?php echo $array[0] ?>
-                </td>
-                <td>
-                    <?php echo $array[1] ?>
-                </td>
-                <td>
-                    <?php echo $array[2] ?>
-                </td>
-                <td>
-                    <?php echo $array[3] ?>
-                </td>
-            </tr>
-    </table>
-
-<?php
-// }
-?> --> 
-
-
-
-
-
-
+    
 </body>
+<script>
+    function del()
+    {
+        document.getElementById("delete").removeAttribute("hidden");
+        document.getElementById("delbtn").removeAttribute("hidden");
+    }
+</script>
 </html>
